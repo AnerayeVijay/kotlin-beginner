ea3c3669c6911f0817f561ce4ab68dcc5069c3f9
Vijay Aneraye <vijay.aneraye@crest.in>
2018-12-30 13:22:39 +0530

    String template examples

---
+++ src/test/kotlin/stringtemplate/StringTemplateDemoTest.kt
@@ -0,0 +1,19 @@
+package stringtemplate
+
+import com.stringtemplate.countLengthOfString
+import com.stringtemplate.sayHello
+import org.junit.Assert
+import org.junit.Test
+
+class StringTemplateDemoTest {
+    @Test
+    fun `say Hello Mak`() {
+        Assert.assertEquals("Hello Mac", sayHello("Mac"))
+
+    }
+
+    @Test
+    fun `count lenght of String "Kotlin"`() {
+        Assert.assertEquals("lenght:=6", countLengthOfString("Kotlin"))
+    }
+}
